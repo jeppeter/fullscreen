f9206c8f25799fffb9ae8db0778b3325a718f230
jeppeter <jeppeter@gmail.com>
2013-07-28 03:09:17 +0800

    make a real capture with GetRenderTargetData

--- Common/scrcap.cpp
+++ Common/scrcap.cpp
@@ -8,7 +8,7 @@ extern "C" int  CaptureFile(IDirect3D9* pD3D,IDirect3DDevice9* pDevice,const cha
 {
     D3DDISPLAYMODE D3DMode;
     HRESULT hr;
-    LPDIRECT3DSURFACE9 pSurface = NULL;
+    LPDIRECT3DSURFACE9 pSurface = NULL,pBackBuffer=NULL;
     LPWSTR pFileName=NULL;
     BOOL bret;
     int ret;
@@ -23,26 +23,44 @@ extern "C" int  CaptureFile(IDirect3D9* pD3D,IDirect3DDevice9* pDevice,const cha
     if (FAILED(hr))
     {
         DEBUG_INFO("could not get adapter mode 0x%08x\n",hr);
-		ret = -(int)GetLastError() ? - (int)GetLastError(): -1;
+        ret = -(int)GetLastError() ? - (int)GetLastError(): -1;
+        goto fail;
+    }
+
+
+    hr = pDevice->GetBackBuffer(0,0,D3DBACKBUFFER_TYPE_MONO,&pBackBuffer);
+    if (FAILED(hr))
+    {
+        DEBUG_INFO("could not get backbuffer\n");
+        ret = -(int)GetLastError() ? - (int)GetLastError(): -1;
         goto fail;
     }
 
     hr = pDevice->CreateOffscreenPlainSurface(D3DMode.Width,
             D3DMode.Height,
-            D3DFMT_A8R8G8B8, D3DPOOL_SCRATCH, &pSurface, NULL);
+            D3DMode.Format, D3DPOOL_SYSTEMMEM, &pSurface, NULL);
     if (FAILED(hr))
     {
         DEBUG_INFO("could not create surface\n");
-		ret = -(int)GetLastError() ? - (int)GetLastError(): -1;
+        ret = -(int)GetLastError() ? - (int)GetLastError(): -1;
         goto fail;
     }
 
+    hr = pDevice->GetRenderTargetData(pBackBuffer,pSurface);
+    if (FAILED(hr))
+    {
+        DEBUG_INFO("could not get render target data\n");
+        ret = -(int)GetLastError() ? - (int)GetLastError(): -1;
+        goto fail;
+    }
+
+#ifdef  UNICODE
     pFileName = new wchar_t[8000];
     bret = MultiByteToWideChar(CP_ACP,NULL,filetosave,-1,pFileName,8000);
     if (!bret)
     {
         DEBUG_INFO("can not save file %s\n",filetosave);
-		ret = -(int)GetLastError() ? - (int)GetLastError(): -1;
+        ret = -(int)GetLastError() ? - (int)GetLastError(): -1;
         goto fail;
     }
 
@@ -51,14 +69,30 @@ extern "C" int  CaptureFile(IDirect3D9* pD3D,IDirect3DDevice9* pDevice,const cha
     if (FAILED(hr))
     {
         DEBUG_INFO("could not save file %s\n",filetosave);
-		ret = -(int)GetLastError() ? - (int)GetLastError(): -1;
+        ret = -(int)GetLastError() ? - (int)GetLastError(): -1;
         goto fail;
     }
+
+#else
+    hr = D3DXSaveSurfaceToFile(filetosave, D3DXIFF_BMP, pSurface, NULL, NULL);
+    if (FAILED(hr))
+    {
+        DEBUG_INFO("could not save file %s\n",filetosave);
+        ret = -(int)GetLastError() ? - (int)GetLastError(): -1;
+        goto fail;
+    }
+#endif
     if (pSurface)
     {
         pSurface->Release();
     }
     pSurface = NULL;
+
+    if (pBackBuffer)
+    {
+        pBackBuffer->Release();
+    }
+    pBackBuffer = NULL;
     if (pFileName)
     {
         delete [] pFileName;
@@ -73,6 +107,11 @@ fail:
         pSurface->Release();
     }
     pSurface = NULL;
+    if (pBackBuffer)
+    {
+        pBackBuffer->Release();
+    }
+    pBackBuffer = NULL;
     if (pFileName)
     {
         delete [] pFileName;
