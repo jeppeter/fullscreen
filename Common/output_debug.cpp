4f61d28ce6957e6e8fe0a1b947c8a5da54e6d82f
jeppeter <jeppeter@gmail.com>
2013-07-28 19:13:08 +0800

    to change the innerdebug in the string

--- Common/output_debug.cpp
+++ Common/output_debug.cpp
@@ -4,48 +4,52 @@
 
 
 
+extern "C" void InnerDebug(char* pFmtStr)
+{
+#ifdef UNICODE
+    LPWSTR pWide=NULL;
+    int len;
+    BOOL bret;
+    len = (int) strlen(pFmtStr);
+    pWide = new wchar_t[len*2];
+    bret = MultiByteToWideChar(CP_ACP,NULL,pFmtStr,-1,pWide,len*2);
+    if (bret)
+    {
+        OutputDebugString(pWide);
+    }
+    else
+    {
+        OutputDebugString(L"can not change fmt string");
+    }
+    delete [] pWide;
+#else
+    OutputDebugString(pFmtStr);
+#endif
+    return ;
+}
+
 extern "C" void DebugOutString(const char* file,int lineno,const char* fmt,...)
 {
-	char* pFmt=NULL;
-	char* pLine=NULL;
-	char* pWhole=NULL;
-	LPWSTR pWChar=NULL;
-	BOOL bret;
-	va_list ap;
+    char* pFmt=NULL;
+    char* pLine=NULL;
+    char* pWhole=NULL;
+    va_list ap;
 
-	pFmt = new char[2000];
-	pLine = new char[2000];
-	pWhole = new char[4000];
+    pFmt = new char[2000];
+    pLine = new char[2000];
+    pWhole = new char[4000];
 
-	_snprintf_s(pLine,2000,1999,"%s:%d\t",file,lineno);
-	va_start(ap,fmt);
-	_vsnprintf_s(pFmt,2000,1999,fmt,ap);
-	strcpy_s(pWhole,4000,pLine);
-	strcat_s(pWhole,4000,pFmt);
+    _snprintf_s(pLine,2000,1999,"%s:%d\t",file,lineno);
+    va_start(ap,fmt);
+    _vsnprintf_s(pFmt,2000,1999,fmt,ap);
+    strcpy_s(pWhole,4000,pLine);
+    strcat_s(pWhole,4000,pFmt);
 
-#ifdef UNICODE
-	pWChar = new wchar_t[8000];
-	bret = MultiByteToWideChar(CP_ACP,NULL,pWhole,-1,pWChar,8000);
-	if (bret)
-	{
-		OutputDebugString(pWChar);
-	}
-	else
-	{
-		OutputDebugString(L"can not change multichar\n");
-	}
-#else
-	OutputDebugString(pWhole);
-#endif
-	delete [] pFmt;
-	delete [] pLine;
-	delete [] pWhole;
+	InnerDebug(pWhole);
+    delete [] pFmt;
+    delete [] pLine;
+    delete [] pWhole;
 
-	if (pWChar)
-	{
-		delete [] pWChar;
-	}
-	pWChar = NULL;
-	return ;
+    return ;
 }
 
